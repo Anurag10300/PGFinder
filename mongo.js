165aa3f34190fb089a792c4ce49b2dbb4529ebd1
Anurag10300 <anurag.panwar.2001@gmail.com>
2021-06-30 12:12:40 +0530

    Add Authentication and initiated MongoDB

---
+++ mongo.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+const password = process.env.DB_USER1_PASS;
+const { MongoClient } = require('mongodb');
+const uri = "mongodb+srv://User1:"+password+"@pgdetails.zbdmq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
--- server.js
+++ server.js
@@ -1,17 +1,55 @@
+
+require('dotenv').config();
+
 const express = require("express");
 const ejs = require('ejs');
 
 const app  = express();
+
+const { requiresAuth } = require('express-openid-connect');
+const { auth } = require('express-openid-connect');
+
+const config = {
+    authRequired: false,
+    auth0Logout: true,
+    secret: process.env.SECRET,
+    baseURL: process.env.BASE_URL,
+    clientID: process.env.CLIENT_ID,
+    issuerBaseURL: process.env.ISSUER_URL
+  };
+  
+
 
 app.set('view engine','ejs');
 
 app.use(express.static(__dirname+'/public'));
 
 
-// for root
-app.get('/',function(req,res){
-    res.render('pages/index.ejs');
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+
+app.get("/signup", (req, res) => {
+  res.oidc.login({
+    authorizationParams: {
+      screen_hint: "signup",
+    },
+  });
 });
+
+
+// req.isAuthenticated is provided from the auth router
+app.get('/', (req, res) => {
+   res.render('pages/index');
+    
+
+});
+
+app.get('/profile', requiresAuth(), (req, res) => {
+  res.send(JSON.stringify(req.oidc.user));
+});
+
 
 // the ADD PG button redirects to new page
 app.post('/', function(req,res){
@@ -19,17 +57,8 @@ app.post('/', function(req,res){
 });
 
 
-// for login page
-app.get('/login',function(req,res){
-    res.render('pages/login.ejs');
-});
-app.post('/login', function(req,res){
-    return res.redirect('/');
-});
-
-
 //for newPG page
-app.get('/newPG', function(req,res){
+app.get('/newPG',requiresAuth(), function(req,res){
     res.render('pages/newPG.ejs');
 });
 
@@ -37,6 +66,19 @@ app.post('/newPG', function(req,res){
     return res.redirect('/');
 })
 
+app.get('/login/callback' , requiresAuth(), function(req,res){
+    return res.redirect('/');
+});
+
+app.get("/profile", requiresAuth(), (req, res) => {
+  res.render("profile", {
+    user: req.oidc.user,
+  });
+});
+
+app.get('/authHome', requiresAuth(), function(req,res){
+    res.render('pages/indexAuthorised');
+});
 
 
 app.listen(3000,function(){
